43b2d076095787fc694cad8cb58fad82b205d702
EGHO-LUCKY <egholucky8@gmail.com>
2022-09-08 16:30:43 -0700

    Add <stdio.h>

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
+
 int main()
 {
-	char var1[] = "Programming is like building a multilingual puzzle";
-	puts(var1);
+	puts("Programming is like building a multilingual puzzle");
 	return 0;
 }
