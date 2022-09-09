03e6be848415395282c1bd2b5a13955cdc548023
EGHO-LUCKY <egholucky8@gmail.com>
2022-09-09 23:49:47 +0000

    Add 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - outputs string except q and e
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x == 'q' | x == 'e');
+		else
+			putchar(x);
+	}
+	putchar('\n');
+}
