d042cae8751f844a21143f77024d005aca743544
EGHO-LUCKY <egholucky8@gmail.com>
2022-09-10 00:15:48 +0000

    Add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - outputs base 10 numbers
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+	putchar('\n');
+	
+	return (0);
+}
