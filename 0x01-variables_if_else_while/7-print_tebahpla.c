cb8e0806689556c213a14dc741133c1990f7537d
EGHO-LUCKY <egholucky8@gmail.com>
2022-09-10 01:35:31 +0000

    Add 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - alphabet in reverse
+ * Return: always false
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
