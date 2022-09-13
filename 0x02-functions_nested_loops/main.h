2d27a5d5bce08153979f319bfbccaf158a9d85a6
EGHO-LUCKY <egholucky8@gmail.com>
2022-09-13 16:18:28 +0000

    Modify main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,32 @@
 #include <stdio.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
+char _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int nld);
+void jack_bauer(void);
+void times_table(void);
+int add(int one, int two);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif 
+
+/* HOLBERTON_H */
 /**
  * main - outputs char
  * Return: always 0
  */
 
 
-int _putchar(char x)
+/*int _putchar(char x)
 {
 	putchar(x);
 	return (0);
-}
+}*/
