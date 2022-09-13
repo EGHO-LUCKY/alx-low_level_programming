de0b1708ba2b7c041501434dcf5029f8aa275bf4
EGHO-LUCKY <egholucky8@gmail.com>
2022-09-13 23:55:25 +0000

    Add 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ */
+
+void times_table(void)
+{
+	int x, y, z;
+	
+	for (x = 0 ; x < 10 ; x++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (y = 0 ; y < 10 ; y++)
+		{
+			z = (x * y);
+			if (z / 10)
+			{
+				_putchar((z / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((z % 10) + '0');
+
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
