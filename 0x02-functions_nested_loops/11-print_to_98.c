a5230402f66f2dac2fdf29a6c860a1100b418da0
EGHO-LUCKY <egholucky8@gmail.com>
2022-09-14 01:02:59 +0000

    Modify 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,17 +8,34 @@
 
 void print_to_98(int n)
 {
-	for (; n <= 98 ; n++)
+	if (n < 98)
 	{
-		if (n < 98)
+		for (; n <= 98 ; n++)
 		{
-			printf("%d", n);
-			printf(", ");
-		}
-		else
-		{
-			printf("%d", n);
-			printf("\n");
+			if (n < 98)
+			{
+				printf("%d", n);
+				printf(", ");
+			}
+			else
+			{
+				printf("%d", n);
+				printf("\n");
+			}
 		}
 	}
+	else
+		for (; n >= 98 ; n--)
+		{
+			if (n > 98)
+			{
+				printf("%d", n);
+				printf(", ");
+			}
+			else
+			{
+				printf("%d", n);
+				printf("\n");
+			}
+		}
 }
