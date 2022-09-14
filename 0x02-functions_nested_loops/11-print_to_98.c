6113d1f2a69c8b6979ab0420b5e644ab4908ba1f
EGHO-LUCKY <egholucky8@gmail.com>
2022-09-14 00:35:02 +0000

    Add 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: starting number
+ */
+
+void print_to_98(int n)
+{
+	for (; n <= 98 ; n++)
+	{
+		if (n < 98)
+		{
+			_putchar(n, '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(n, '0');
+			_putchar('\n');
+		}
+	}
+}
