2a5db8ca91cef33838df4314ab315587fcbb8aa0
EGHO-LUCKY <egholucky8@gmail.com>
2022-09-15 15:56:13 +0000

    Modify 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,15 +10,15 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	/* Start an infinite loop */
+	
 	i = 0;
-	/**
-	 * while (i < 10)
-	 * {
-	 *	putchar(i);
-	 * } 
-	 */ 
+	
+	/* while (i < 10) */
+	{
+		putchar(i);
+	} 
+	 
 	printf("Infinite loop avoided! \\o/\n");
-	/* Return: always 0 */
+	
 	return (0);
 }
